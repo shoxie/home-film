c917b3dc71a7c49552bbbef4475939250031186d
shoxie <19520377@gm.uit.edu.vn>
2024-07-08 00:09:25 +0700

    overhaul

--- src/apis/get.ts
+++
@@ -1,9 +0,0 @@
-import { axiosInstance } from "@/lib/initRequest";
-
-export const getMoviesChanges = (pages: number) => {
-  return axiosInstance.get(`movie/changes?page=${pages}`);
-};
-
-export const getOneMovie = (id: number) => {
-  return axiosInstance.get(`movie/${id}`)
-}
--- src/pages/movies/top-rated.tsx
+++
@@ -1,9 +0,0 @@
-const TopRatedMovies = () => {
-    return (
-        <div>
-
-        </div>
-    )
-}
-
-export default TopRatedMovies
